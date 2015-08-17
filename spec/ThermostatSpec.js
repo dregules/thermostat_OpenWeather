c33981c2defabe0fb7077dfbfcaf925e56d8855c
Brandon Graves <bgraves14@gmail.com>
2015-08-17 17:28:58 +0100

    first test passing

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,7 +1,9 @@
 describe('Thermostat', function() {
 
+  var thermostat;
+
   it('has a default temperature of 20', function() {
     thermostat = new Thermostat();
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.temperature()).toEqual(20);
   });
 });
